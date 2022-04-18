0ebf3b11db96aebbe6051fdab63aea181bed04d4
Danu-B <dbulom12@gmail.com>
2022-04-18 14:41:07 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+puts("\"Proggraming is like building a muktilingual puzzle");
+return (0);
+}
