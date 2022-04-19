8925e1606a73f4f1af980fddae8f889162c62846
Danu-B <dbulom12@gmail.com>
2022-04-19 01:52:25 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
 *main - entry point
 *
@@ -6,6 +6,6 @@ include <stdio.h>
 */
 int main(void)
 {
-puts("\"Programming is like building a muktilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
