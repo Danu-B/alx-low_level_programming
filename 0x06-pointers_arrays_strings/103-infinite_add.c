7934243be950d7fc8ed0009ecb4f9e0aaf10c883
Danu-B <51906814+Danu-B@users.noreply.github.com>
2022-04-21 15:23:46 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,37 +1,52 @@
 
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * print_number - printing number with puts
+ * infinite_add - Adds to numbers in a string
+ * @n1: The number
+ * @n2: The number
+ * @r: The result
+ * @size_r: The qty
  *
- * @n: input int
+ * Return: none
  */
-void print_number(int n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/*Declaring variables*/
-	int count = 0, pow = 1;
-	unsigned int num = n;
+	char t;
+	int  i, j, k, size_1, size_2, a, b, c =  0;
 
+	size_1 = strlen(n1);
+	size_2 = strlen(n2);
 
-	if (n < 0) /*Evaluate this condition*/
+	if (size_1 > (size_r - 1) || size_2 > (size_r - 1))
+		return (0);
+
+	for (i = --size_1, j = --size_2, k = 0; i >= 0 || j >= 0; k++)
 	{
-		_putchar('-');
-		num = -n;
+		if (i >= 0)
+			a = n1[i--] - 48;
+		else
+			a = 0;
+		if (j >= 0)
+			b = n2[j--] - 48;
+		else
+			b = 0;
+		r[k] = (a + b + c) % 10 + 48;
+		c = (a + b + c) / 10;
+		if (k >= (size_r - 2))
+			return (0);
 	}
-	while (n != 0)
+	if (c > 0)
+		r[k++] = c + 48;
+	r[k] = '\0';
+	for (i = 0; i < (k / 2); i++)
 	{
-		n /= 10;
-		count++;
+		t = r[i];
+		r[i] = r[k - i - 1];
+		r[k - i - 1] = t;
 	}
-	while (count > 1)
-	{
-		pow *= 10;
-		count--;
-	}
-	while (pow >= 1)
-	{
-		_putchar(num / pow % 10 + '0');
-		pow /= 10;
-	}
+
+	return (r);
 }
