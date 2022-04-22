e4faac5644555392a5e822c0b3a0ebde67478da6
Danu-B <51906814+Danu-B@users.noreply.github.com>
2022-04-22 22:41:25 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,51 @@
 
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * infinite_add - Adds to numbers in a string
- * @n1: The number
- * @n2: The number
- * @r: The result
- * @size_r: The qty
- *
- * Return: none
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+void print_buffer(char *b, int size)
 {
-	char t;
-	int  i, j, k, size_1, size_2, a, b, c =  0;
+	int o, j, i;
 
-	size_1 = strlen(n1);
-	size_2 = strlen(n2);
+	o = 0;
 
-	if (size_1 > (size_r - 1) || size_2 > (size_r - 1))
-		return (0);
-
-	for (i = --size_1, j = --size_2, k = 0; i >= 0 || j >= 0; k++)
+	if (size <= 0)
 	{
-		if (i >= 0)
-			a = n1[i--] - 48;
-		else
-			a = 0;
-		if (j >= 0)
-			b = n2[j--] - 48;
-		else
-			b = 0;
-		r[k] = (a + b + c) % 10 + 48;
-		c = (a + b + c) / 10;
-		if (k >= (size_r - 2))
-			return (0);
+		printf("\n");
+		return;
 	}
-	if (c > 0)
-		r[k++] = c + 48;
-	r[k] = '\0';
-	for (i = 0; i < (k / 2); i++)
+	while (o < size)
 	{
-		t = r[i];
-		r[i] = r[k - i - 1];
-		r[k - i - 1] = t;
-	}
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
 
-	return (r);
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
